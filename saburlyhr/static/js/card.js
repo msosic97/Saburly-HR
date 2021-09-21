dd446c95b5cf9441491ac73e8846201e56aad00d
msosic1 <msosic1@etf.unsa.ba>
2021-09-21 09:41:34 +0200

    Dodana kartica u static

--- saburlyhr/static/js/card.js
+++ saburlyhr/static/js/card.js
@@ -1,4 +1,14 @@
-function showDiv() {
+function showDiv1() {
+    document.getElementById('second-container').style.display = "none";
+    document.getElementById('first-container').style.display = "block";
+}
+
+function showDiv2() {
     document.getElementById('card').style.display = "block";
 }
 
+function showDiv3() {
+    document.getElementById('first-container').style.display = "none";
+    document.getElementById('second-container').style.display = "block";
+    document.getElementById('second-container').innerHTML= "Biti ce bolje aBd, ali ne vjerujem. Provjerite je li placena vezifa!";
+}
