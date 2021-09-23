8d3ade58a88318d2c03437d4c3680567be99f3ab
ismailsosic <ismailsosic@gmail.com>
2021-09-24 01:34:59 +0200

    make code simplier

--- saburlyhr/urls.py
+++ saburlyhr/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('account.urls')),
-    # path('cards/', include('cards.urls')),
+    path('', include('pages.urls')),
+    path('dashboard/', include('account.urls')),
     path('admin/', admin.site.urls),
 ]
