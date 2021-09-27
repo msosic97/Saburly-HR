e984fd6ac097925e625f01576b7a6252f8491b94
ismailsosic <ismailsosic@gmail.com>
2021-09-28 00:24:12 +0200

    add form files and dashboard.html changes

---
+++ cards/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class CardForm(forms.Form):
+    date_from = forms.DateField(label='Date from', widget = forms.TextInput(attrs={
+        'type': 'date'
+    }))
+    date_to = forms.DateField(label='Date from', widget = forms.TextInput(attrs={
+        'type': 'date'
+    }))
+    description = forms.CharField(label='Description', widget = forms.Textarea({
+        'cols':'40',
+        'rows':'5',
+    }))
